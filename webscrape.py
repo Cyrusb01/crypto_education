93a150a2fb57283eec49f040ba45435d1604b3c8
Cyrus Buhariwalla <cyrusb01@outlook.com>
2021-11-10 12:13:06 -0600

    news working

--- webscrape.py
+++ webscrape.py
@@ -6,6 +6,8 @@ https://towardsdatascience.com/web-scraping-basics-82f8b5acd45c
 """
 from bs4 import BeautifulSoup
 import requests 
+import pandas as pd
+import re 
 
 
 url = "https://www.upfolio.com/100-coins-explained-backup"
@@ -15,7 +17,7 @@ content=req.text
 
 # print(content)
 
-soup=BeautifulSoup(content)
+soup=BeautifulSoup(content, features="lxml")
 
 # print(soup)
 
@@ -23,7 +25,30 @@ names = soup.findAll("h3", {"class": "coinname"})
 links = soup.findAll("a", {"class": "coinlink1"})
 logo = soup.findAll("img",  {"class": "coinlogo"})
 description = soup.findAll("p", {"class": "cointext"})
+
+names = [x.text for x in names]
+coin_name = [x[:x.find(" ")] for x in names]
+print(coin_name)
+
+tickers = [re.search('\(([^)]+)', x).group(0).replace('(', "") for x in names]
+# print(tickers)
+
+links = [x.get('href') for x in links]
+logo = [x.get('src') for x in logo]
+description = [x.text for x in description]
+
 print(names)
-print(links)
-print(logo)
-print(description)
+# print(links)
+# print(logo)
+# print(description)
+
+df = pd.DataFrame(list(zip(tickers, coin_name, links, logo, description)),
+               columns =['Tickers', 'Name', 'Link', "Logo", "Description"])
+
+
+df = df.set_index("Tickers")
+
+
+print(df)
+
+df.to_csv("Coins.csv")
