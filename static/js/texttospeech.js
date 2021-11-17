beb2d0339ce0376c5d5763061e007faabad24337
Adam <adamcevallos238@gmail.com>
2021-11-17 11:55:28 -0600

    initial speech commit

---
+++ static/js/texttospeech.js
@@ -0,0 +1,20 @@
+let speech = new SpeechSynthesisUtterance();
+const synth = window.speechSynthesis;
+const say = (text) => {
+  synth.cancel();
+  speech.lang = "en";
+  speech.text = text;
+  synth.speak(speech);
+}
+
+document.querySelectorAll('.speak').forEach(para => {
+    let speaker = document.createElement('span');
+    const text = para.textContent;
+    speaker.classList = ['speaker'];
+    speaker.style.cursor = "pointer";
+    speaker.innerHTML = '&#128264';
+    speaker.addEventListener('click', () => say(text));
+    para.appendChild(speaker);
+})
+
+window.onbeforeunload = () => synth.cancel();
